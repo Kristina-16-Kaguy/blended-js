0c5895fda694bb1a7d31a7d1cac88e828ab9f4e2
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-12 18:08:19 +0300

    lesson4, part1

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,25 +1,109 @@
 // Завдання 1
 
+// const { createElement } = require("react");
+
 // 1 - отримай body елемент і виведи його в консоль;
+const bodyEl = document.querySelector("body");
+console.log(bodyEl);
+
 // 2 - отримай елемент id="title" і виведи його в консоль;
+const id = document.querySelector("#title");
+console.log(id);
+
 // 3 - отримай елемент class="list" і виведи його в консоль;
+const list = document.querySelector(".list");
+console.log(list);
+
 // 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
+const topicEl = document.querySelectorAll("[data-topic]");
+console.log(topicEl);
 // 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const dataEl = document.querySelector("[data-topic]");
+console.log(dataEl);
+console.log(topicEl[0]);
+
 // 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const listElements = document.querySelectorAll("[data-topic]");
+const lastListElement = listElements[listElements.length - 1];
+console.log(lastListElement);
+console.log(topicEl[topicEl.length - 1]);
+
 // 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
+const h1 = document.querySelector("h1");
+const next = h1.nextElementSibling;
+console.log(next);
+
 // 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
+const headerEl = document.querySelectorAll("h3");
+console.log(headerEl);
+headerEl.forEach((el) => console.log(el.textContent));
+
 // 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка на червоний колір
+// const h3 = document.querySelectorAll("h3");
+// console.log(h3);
+const h3El = headerEl.forEach((h3) => h3.classList.add("active"));
+
 // 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+const listItem = document.querySelector("li[data-topic=navigation]");
+console.log(listItem);
 // 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
+listItem.style.backgroundColor = "yellow";
+
 // 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
-// 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
+const paragraph = listItem.querySelector("p");
+paragraph.textContent = "Я змінив тут текст!";
+
+// 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається
+// у змінній currentTopic і виведи його в консоль;
+const currentTopic = "manipulation";
+const currentTopicEl = document.querySelector(`[data-topic="${currentTopic}"]`);
+console.log(currentTopicEl);
 // 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
+currentTopicEl.style.backgroundColor = "blue";
+
 // 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
+const titleEl = document.querySelector(".completed");
+console.log(titleEl);
+
 // 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
-// 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
-// 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
+const newEl = titleEl.closest("li");
+newEl.remove();
+
+// 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа
+// (Document Object Model)"
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "Об'єктна модель документа (Document Object Model)";
+h1.insertAdjacentElement("afterend", newParagraph);
+
+// 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) -
+//  "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити
+//  всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
+// const liEl = document.createElement("li");
+// const h3Element = document.createElement("h3");
+// const pEl = document.createElement("p");
+// h3Element.textContent = "Властивість innerHTML";
+// pEl.textContent =
+//   "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
+// liEl.append(h3El, pEl);
+// list.append(liEl);
+
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
+const createNewVersion = `
+  <li>
+    <h3>Властивість innerHTML</h3>
+    <p>
+      Ще один спосіб створити DOM-елементи і помістити їх в дерево - це
+      використовувати рядки з тегами і дозволити браузеру зробити всю важку
+      роботу
+    </p>
+  </li>
+`;
+
+list.insertAdjacentHTML("beforeend", createNewVersion);
+
 // 20 - очисти список
+const UlEl = document.querySelector(".list");
+UlEl.innerHTML = "";
 
 // Завдання 2
 // Створіть контейнер div (з класом number-container) в HTML-документі
